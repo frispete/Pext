58930e56727ef3c187357bb8af98eef01efb0af5
Hans-Peter Jansen <hpj@urpla.net>
2017-04-03 12:02:28 +0200

    Too many dummies all around...

--- generate_translations_ts.sh
+++ generate_translations_ts.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/sh -x
 
 # lupdate-qt5 does not support Python and pylupdate5 does not support QML
-pylupdate5 pext/__main__.py -ts pext/i18n/pext.dummy.py.ts
-lupdate-qt5 pext/qml/* -ts pext/i18n/pext.dummy.qml.ts
+pylupdate5 pext/__main__.py -ts pext/i18n/pext.py.ts
+lupdate-qt5 pext/qml/* -ts pext/i18n/pext.qml.ts
 
-# Sed fixes for pylupdate5's broken behaviour
-sed -i 's#<location filename="__main__.py"#<location filename="../__main__.py"#g' pext/i18n/pext.dummy.py.ts
+# Sed fixes for pylupdate5's broken behaviour (fixed lately)
+sed -i 's#<location filename="__main__.py"#<location filename="../__main__.py"#g' pext/i18n/pext.py.ts
 
 # Merge both together in a new dummy file
-lconvert -i pext/i18n/pext.dummy.py.ts pext/i18n/pext.dummy.qml.ts -o pext/i18n/pext.dummy.ts
+lconvert -i pext/i18n/pext.py.ts pext/i18n/pext.qml.ts -o pext/i18n/pext.ts
 
 # Remove tmp files
-rm -f pext/i18n/pext.dummy.py.ts pext/i18n/pext.dummy.qml.ts
+rm -f pext/i18n/pext.py.ts pext/i18n/pext.qml.ts
